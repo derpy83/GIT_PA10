a94b9b38b4ad7a1ccb52b27cc859b16cee7c31fc
derpy83 <jacobchua00@gmail.com>
2020-06-18 12:09:07 +0800

    Updated Chua jia jun

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public static GameManager thisManager = null;  
-    [SerializeField] private Text Txt_Score = null;
+    
     [SerializeField] private Text Txt_Message = null;
-    private int Score = 0;
+    
 
     void Start()
     {
@@ -22,18 +22,14 @@ public class GameManager : MonoBehaviour
             StartGame();
     }
 
-    public void UpdateScore(int value)
-    {
-        Score += value;
-        Txt_Score.text = "SCORE : " + Score;
-    }
+    
 
     private void StartGame()
     {
-        Score = 0;
+        
         Time.timeScale = 1;
         Txt_Message.text = "";
-        Txt_Score.text = "SCORE : 0";
+        
     }
 
     public void GameOver()
@@ -41,5 +37,9 @@ public class GameManager : MonoBehaviour
         Time.timeScale = 0;
         Txt_Message.text = "GAMEOVER! \nPRESS ENTER TO RESTART GAME.";
         Txt_Message.color = Color.red;
+    }
+    public void restart()
+    {
+        SceneManager.LoadScene("SampleScene");
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
+    public float jump = 100f;
+    private Rigidbody birdrigid;
+    [SerializeField] private Text Txt_Score = null;
 
+    private int Score = 0;
+   
     void Start()
     {
+        Score = 0;
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
+
+        birdrigid = GetComponent<Rigidbody>();
+        Txt_Score.text = "SCORE : 0";
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
             thisAnimation.Play();
+        }
+            
     }
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Bound")
+        {
+            SceneManager.LoadScene("Gameover");
+        }
+        if (other.gameObject.tag == "score")
+        {
+            Score += 1;
+            Txt_Score.text = "SCORE : " + Score;
+        }
+        if (other.gameObject.tag == "block")
+        {
+            SceneManager.LoadScene("Gameover");
+        }
+    }
+    public void Jump()
+    {
+        birdrigid.velocity = Vector3.up * jump;
+    }
+    
 }
